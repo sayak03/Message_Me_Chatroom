edced7377904c89825cf093a48dbf33340413a57
Sayak Rana <sayakrana03@gmail.com>
2023-06-14 16:39:36 +0530

    add authentication methods and Add sessions create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
 	def new
 	end
+
+	def create
+	  user = User.find_by(name: params[:session][:name])
+	  if user && user.authenticate(params[:session][:password])
+	    session[:user_id] = user.id
+	    flash[:alert] = "You have successfully logged in"
+	    redirect_to root_path
+	  else
+	    flash.now[:alert] = "There was something wrong with your login information"
+	    render :new
+	  end
+	end
+	def destroy
+	  session[:user_id] = nil
+	  flash[:alert] = "You have successfully logged out"
+	  redirect_to login_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :users
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 end
